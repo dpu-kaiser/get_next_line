47e9ca74964ea262dde7df2a6d07b5ef15de9230
Dominik Kaiser <dkaiser@1-C-9.42heilbronn.de>
2024-03-15 15:05:31 +0100

    Setup project

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 14:14:07 by dkaiser           #+#    #+#             */
+/*   Updated: 2024/03/15 14:16:32 by dkaiser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+#endif // GET_NEXT_LINE_H
