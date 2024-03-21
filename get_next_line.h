18df570cd98093147845adfa822db424109a343c
Dominik Kaiser <dkaiser@1-C-5.42heilbronn.de>
2024-03-21 15:23:26 +0100

    Remove old code and add str_realloc()
    Code doesn't work at all after this but I have to refactor it anyways.

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 14:14:07 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/03/21 11:24:39 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/03/21 15:22:54 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@
 # endif
 
 char	*get_next_line(int fd);
+char * str_realloc(char *str, size_t size);
 #endif // GET_NEXT_LINE_H
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,30 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 14:14:59 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/03/15 14:15:08 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/03/21 15:22:40 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char * str_realloc(char *str, size_t size)
+{
+	char *result;
+	size_t i;
+
+	result = malloc(size);
+	if (!result)
+	{
+		free(str);
+		return NULL;
+	}
+	i = 0;
+	while (str[i])
+	{
+		result[i] = str[i];
+		i++;
+	}
+	while (i < size)
+		result[i++] = '\0';
+	return result;
+}
