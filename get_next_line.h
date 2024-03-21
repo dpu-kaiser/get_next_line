94b47301589acbc1ac7e75dd93b183a796cc9bad
Dominik Kaiser <dkaiser@1-C-5.42heilbronn.de>
2024-03-21 13:18:23 +0100

    Add core functionality

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 14:14:07 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/03/15 14:16:32 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/03/21 11:24:39 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 #endif // GET_NEXT_LINE_H
